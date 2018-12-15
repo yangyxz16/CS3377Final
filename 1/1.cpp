f99ef2d264a3ac47429a31c0b7f867bb616c5ab1
Xizhen Yang <yangyxz16@gmail.com>
2018-12-14 18:51:57 -0600

    Problem 1: C++: calculate pai and I made a make file for 1.cpp

--- 1/1.cpp
+++ 1/1.cpp
@@ -1,20 +1,41 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<time.h>
+#include<math.h>
+#include<cmath>
+#include <ctime>
 
 double randomGenerator(double min, double max);
 
 int main(int argc, char *argv[])
 {
-	int n = 10;
+	//clock_t tStart = clock();
+	int n = 10000;	// Number of points.
+	
 	double r[n];
+	double x;
+	double y;
+	double d;
+	
+	int numInC;	// Number points in quarter unit circle.
 
 	srand(time(NULL));
 
 	for (int i = 0; i < n; i++) {
-		r[i] = randomGenerator(0, 1);
-		printf("%g\n", r[i]);
+		x = randomGenerator(0, 1);
+		y = randomGenerator(0, 1);
+		d = sqrt(pow(x,2) + pow(y,2));
+		if (d <= 1) {
+			numInC++; 
+		}
+
 	}
+	
+	double pai = 4 * (numInC / (double)n);
+	printf("%g\n", pai);
+	//int stop_s=clock();
+	//printf("Time taken: %.9fs\n", (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000);
+	//printf("Time taken: %.9fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC*1000000);
 	return 0;
 }
 
