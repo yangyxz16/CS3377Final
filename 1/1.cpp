31e1a2492c5d3f9f1960a53079eb75ec1db0152e
Xizhen Yang <yangyxz16@gmail.com>
2018-12-15 20:47:32 -0600

    Problem 1: C++: get estimate value/deviation/%error

--- 1/1.cpp
+++ 1/1.cpp
@@ -5,45 +5,73 @@
 #include <cmath>
 #include <ctime>
 #include <assert.h>
+#include <unistd.h>
+#include <cstdlib>
 #include "mytools.h"
 
+double paiLoop(int point); 
 //double randomGenerator(double min, double max);
 int main(int argc, char **argv)
 {
-	//clock_t tStart = clock();
-	int n = 10000;	// Number of points.
+	int Start = clock();
 	
-	double r[n];
-	double x;
-	double y;
-	double d;
+	int points = 10000;	// Number of points.
 	
-	int numInC;	// Number points in quarter unit circle.
+	double r[10];
+	double sum = 0;
+	double sd = 0;
 
-	//srand(time(NULL));
-	assert(init_mytool() == 0);
+	for (int i = 0; i < 10; i++) {
+		r[i] = paiLoop(points);
+		sum += r[i];
+		sleep(1);
+	}
 	
-	for (int i = 0; i < n; i++) {
-		//x = randomGenerator(0, 1);
-		//y = randomGenerator(0, 1);
-		x = get_random_number();
-		y = get_random_number();
-		d = sqrt(pow(x,2) + pow(y,2));
-		if (d <= 1) {
-			numInC++; 
-		}
+	double avg = sum / 10;
+
+	for (int i = 0; i < 10; ++i) {
+		sd += pow(r[i] - avg, 2);
 	}
 
-	finalize_mytool();
+	sd = sqrt(sd / 10);
 
-	double pai = 4 * (numInC / (double)n);
-	printf("%g\n", pai);
-	//int stop_s=clock();
+	double error = (avg - 3.14159265359) * 100 / 3.14159265359 ;
+	
+	if (error < 0) {
+		error = error * -1;
+	}		
+
+	printf("Average of 10 trails is: %g\n", avg);
+	printf("Standard Deviation is: %g\n", sd);
+	printf("Percentage error is: %g percent\n", error);
+	int stop_s=clock();
 	//printf("Time taken: %.9fs\n", (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000);
-	//printf("Time taken: %.9fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC*1000000);
+	//printf("Time taken: %.9fs\n", (double)(clock() - tStart)/CLOCKS_PER_SEC*1000);
+	printf("Time taken: %f\n", (float)Start / CLOCKS_PER_SEC);
 	return 0;
 }
 
+double paiLoop(int point)
+{
+	double x;
+	double y;
+	double d;
+	int numInC = 0;     // Number points in quarter unit circle.
+	assert(init_mytool() == 0);
+	
+	for (int i = 0; i < point; i++) {
+		x = get_random_number();
+		y = get_random_number();
+		d = sqrt(pow(x,2) + pow(y,2));
+		if (d <= 1) { 
+			numInC++; 
+		}
+	}
+	finalize_mytool();
+	
+	double pai = 4 * (numInC / (double)point); 
+	return pai;
+}
 
 int init_mytool()
 {
