097e4192fe436bcc2287daf6a4af1bc1a1fb8c29
Xizhen Yang <yangyxz16@gmail.com>
2018-12-16 21:44:36 -0600

    Problem 2: dealer creates 9 x 1000 trials run from 10% to 90%

--- 2/build_me.sh
+++ 2/build_me.sh
@@ -1,8 +1,3 @@
-Compile hand.cpp:
--I$HOME/local/gsl-2.5/include hand.cpp -lgsl -lgslcblas -L$HOME/local/gsl-2.5/lib
+gcc -o hand -I$HOME/local/gsl-2.5/include hand.cpp -lgsl -lgslcblas -L$HOME/local/gsl-2.5/lib
+gcc -o dealer dealer.cpp
 
-Change "a.out"(the compiled hand.cpp) to "hand":
-cp a.out hand
-
-Export library:
-export LD_LIBRARY_PATH=$HOME/local/gsl-2.5/lib
--- 2/dealer.cpp
+++ 2/dealer.cpp
@@ -8,9 +8,9 @@
 #include <fcntl.h>
 #include <errno.h>
 #include <string.h>
-//#include <iostream>
-
-void verbose();
+void createProcess(int percent, int numOfT, bool v);
+void create9000Process(int numOfT);
+void checkPT(int percent, int numOfT);
 
 int main(int argc, char **argv)
 {
@@ -40,15 +40,7 @@ int main(int argc, char **argv)
 
 	numOfT = atoi(argv[optind]);
 
-	if (percent > 100) {
-		fprintf(stderr, "The percentage cannot be larger than 100!\n");
-		exit(-1);
-	}
-
-	if (numOfT == 0) {
-		fprintf(stderr, "The number of trails is invaild!\n");
-		exit(-1);
-	}
+	checkPT(percent, numOfT);
 
 	printf("%s\n", fileName);
 
@@ -57,55 +49,121 @@ int main(int argc, char **argv)
 	fd = open(fileName, O_RDWR | O_CREAT | O_APPEND, 0644);
 	if (fd < 0) { perror("r1"); exit(1); }
 
-	sz = write(fd, "cs3377\n", strlen("cs3377\n"));
+	write(fd, "cs3377\n", strlen("cs3377\n"));
 
-	printf("called write(%d, \"cs3377\\n\", %d), which returned %d\n",fd, strlen("cs3377\n"), sz);
 
 	close(fd);
 
 
+	createProcess(percent, numOfT, verbose);
+	create9000Process(numOfT);
+	return 0;
+}
 
-/*
+void createProcess(int percent, int numOfT, bool v) {
 	int i, stat;
-	pid_t pid; 
+	pid_t pid;
+	int win = 0;	// Counter for success.
+	int loss = 0;   // Counter for failure.
+	int total = 0;	// Total processes created.
+	char arg1[20];
+	snprintf(arg1, sizeof(arg1), "%d", percent);     // Change percent(int) to string.
 	
-	int p[2];
-
-	int win;
-	int loss;
-
-	for (i=0; i<numOfT; i++) { 
+	for (i=0; i<numOfT; i++) {
 		pid = fork();
 
 		if (pid == 0) {
 			sleep(1);
-			stat = execv("./hand", &argv[1]);
+			stat = execl("./hand", "./hand", arg1, NULL); 
 			exit(0);
-		} else {
-			if (verbose) {
-                      		 printf("PID %d return\n ", pid);
+		 } else {
+		 	if (v) {
+				printf("PID %d return\n ", pid);
 			}
-			
+
 			pid = wait(&stat);
-			
-			if (WEXITSTATUS(stat) == 0) {		// Failure
+
+			if (WEXITSTATUS(stat) == 0) {       
 				win++;
 			} else {
 				loss++;
 			}
+			
+			total++;
 		}
 	}
-	win = numOfT - loss;
-	double lossP = (double)loss / (double)numOfT;
-	lossP = lossP * 100;
 	
-	int lossPP = (int)lossP;
-	int winPP = 100 - lossPP;
-	//printf("Success %d\n", win);
-	//printf("Failure %d\n", loss);
-	printf("Created %d processes.\n", numOfT);
+	win = total - loss;
+	double lossP = (double)loss / (double)total;
+	lossP = lossP * 100;
+	int lossPP = (int)lossP;        // Percentage of failure.
+	int winPP = 100 - lossPP;       // Percentage of success;
+
+	printf("Created %d processes.\n", total);
 	printf("Success -  %d%%\n", winPP);
 	printf("Failure -  %d%%\n", lossPP);
-*/			
-	return 0;
 }
+
+void create9000Process(int numOfT) {
+	int stat;
+	pid_t pid; 
+	int startP = 10; //Starting percentage.
+	char arg1[20];
+
+	while (startP <= 90) {
+		int win = 0;	// Counter for success.
+		int loss = 0;   // Counter for failure.
+		int total = 0;  // Total processes created.
+		
+		snprintf(arg1, sizeof(arg1), "%d", startP);	// Change startP(int) to string.
+		for (int i=0; i<numOfT; i++) { 
+			pid = fork();
+
+			if (pid == 0) {
+				sleep(1);
+				stat = execl("./hand", "./hand", arg1, NULL);	// Starting at 10 percent, 
+								// incrementing by 10 until get to input percent
+				exit(0);
+			} else {	
+				pid = wait(&stat);
+			
+				if (WEXITSTATUS(stat) == 0) {		// Failure
+					win++;
+				} else {
+					loss++;
+				}
+
+				total++;
+			}
+		}
+		
+		win = total - loss;
+		double lossP = (double)loss / (double)total;
+		lossP = lossP * 100;
+		int lossPP = (int)lossP;        // Percentage of failure.
+		int winPP = 100 - lossPP;       // Percentage of success;
+		
+		printf("Percentage: %d%%\n", startP);
+		printf("Created %d processes.\n", total);
+		printf("Success -  %d%%\n", winPP);
+		printf("Failure -  %d%%\n", lossPP);
+		startP+=10;
+	
+	}
+}
+
+
+void checkPT(int percent, int numOfT)
+{	
+	if (percent > 100) {
+		fprintf(stderr, "The percentage cannot be larger than 100!\n");
+		exit(-1);
+	}
+
+	 if (numOfT == 0) {
+	 	fprintf(stderr, "The number of trails is invaild!\n");
+		exit(-1);
+	}
+}
+
+
